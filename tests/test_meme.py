22d5e6671d6a24c135c0fe815a6ebd37d788ddec
Trent Nielsen <trentnielsen84@gmail.com>
2019-07-31 18:20:01 -0700

    moar trial and error

--- tests/test_meme.py
+++ tests/test_meme.py
@@ -1,10 +1,10 @@
-from my_project.memegen import creatememe
+import my_project
 
 #pwd = myCmd = os.popen('pwd').read()
 #sys.path.append(pwd)
 
 def validate_url():
-    meme = creatememe('this should', 'work with a test')
+    meme = my_project.creatememe('this should', 'work with a test')
     print(meme)
     assert 'img' in meme
 
